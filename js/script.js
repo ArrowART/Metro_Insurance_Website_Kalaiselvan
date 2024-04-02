aa78e4cd3edc3b3697a47c74463b307612e62a03
kalaiselvan37 <kalaiselvan.tcz@gmail.com>
2024-04-02 16:12:36 +0530

    new get a quote design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// nav bar
 var toggleBtn = document.getElementById('toggle');
 var collapseMenu = document.getElementById('collapseMenu');
 
@@ -10,3 +11,40 @@ function handleClick() {
 }
 
 toggleBtn.addEventListener('click', handleClick);
+
+// scroll
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector('a[href="#contact"]').addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector('#contact').scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+  // Scroll to Reserve now section
+  document.querySelector('a[href="#services"]').addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector('#services').scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+  // Scroll to About Us section
+  document.querySelector('a[href="#aboutus"]').addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector('#aboutus').scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+    // Scroll to Home section
+    document.querySelector('a[href="#home"]').addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector('#home').scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+  document.querySelector('a[href="#getaquote"]').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector('#getaquote').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+});
