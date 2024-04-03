62187bbc5849be82106964143edd959e3b59eb5f
kalaiselvan37 <kalaiselvan.tcz@gmail.com>
2024-04-03 18:35:31 +0530

    new changes

--- js/script.js
+++ js/script.js
@@ -80,12 +80,12 @@ function myModel() {
   }
 }
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzZreWK5sTk2wNJCUsCXhO3FZf1BzMb2RLAUGhbf6WdbE7RD-KRS0gUcB-t_fA-LR1tnw/exec'
-const form = document.forms['contact-form']
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully."))
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxU00nCUzD9YLCzybkojxVsKOHlHGPh0PxVqdNWXrMRdL91PJjC-gYGe0HyvNIlIhTf1A/exec'
+  const form = document.forms['submit-to-google-sheet']
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => alert("Thank you! your form is submitted successfully."))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
-})
+  })
