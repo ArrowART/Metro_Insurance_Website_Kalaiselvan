2eb374ee396c811c0424fc403279d682e4764a65
kalaiselvan37 <kalaiselvan.tcz@gmail.com>
2024-04-04 18:09:14 +0530

    new pdf section added

--- js/script.js
+++ js/script.js
@@ -80,21 +80,78 @@ function myModel() {
   }
 }
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxU00nCUzD9YLCzybkojxVsKOHlHGPh0PxVqdNWXrMRdL91PJjC-gYGe0HyvNIlIhTf1A/exec'
-  const form = document.forms['submit-to-google-sheet']
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(response => alert("Thank you! your form is submitted successfully."))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-  });
+function myModel1() {
+  let modal = document.getElementById("modal1");
+  if (modal.classList.contains('hidden')) {
+      modal.classList.remove('hidden');
+      modal.classList.add('animate-zoomIn');
+  } else {
+      modal.classList.remove('animate-zoomIn');
+      modal.classList.add('hidden');
+  }
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxU00nCUzD9YLCzybkojxVsKOHlHGPh0PxVqdNWXrMRdL91PJjC-gYGe0HyvNIlIhTf1A/exec';
+const form = document.forms['submit-to-google-sheet'];
+const loadingIndicator = document.querySelector('.loading');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  loadingIndicator.style.display = 'block';
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      loadingIndicator.style.display = 'none';
+      alert("Thank you! your form is submitted successfully.");
+      window.location.reload();
+    })
+    .catch(error => {
+      console.error('Error!', error.message);
+      loadingIndicator.style.display = 'none';
+    });
+});
 
   const form1 = document.forms['submit-to-google-sheet1']
+  const loadingIndicator1 = document.querySelector('.loading1')
   form1.addEventListener('submit', e => {
-    e.preventDefault()
+    e.preventDefault();
+    loadingIndicator.style.display = 'block' ;
     fetch(scriptURL, { method: 'POST', body: new FormData(form1) })
-    .then(response => alert("Thank you! your form is submitted successfully."))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
+    .then(response => {
+      loadingIndicator.style.display = 'none';
+      alert("Thank you! your form is submitted successfully.");
+      window.location.reload();
+    })
+    .catch(error => {
+      console.error('Error!', error.message);
+      loadingIndicator.style.display = 'none';
+    });
   })
+
+  // WhatsApp Message
+
+  function sendFormData(event) {
+    event.preventDefault();
+    var formData = new FormData(document.forms['submit-to-google-sheet3']);
+    var name = formData.get('Full-name1');
+    var email = formData.get('Email1');
+    var insurance = formData.get('Insurance1');
+    var message = formData.get('message1');
+  
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Insurance:', insurance);
+    console.log('Message:', message);
+  
+    var phonenumber = "8124213593"; 
+    var url = "https://wa.me/" + phonenumber + "?text=" +
+      "*Name:* " + name + "%0a" +
+      "*Email:* " + email + "%0a" +
+      "*Insurance:* " + insurance + "%0a" +
+      "*Message:* " + message +
+      "%0a%0a" +
+      "";
+  
+    window.open(url, '_blank').focus();
+  }
+  
+  
